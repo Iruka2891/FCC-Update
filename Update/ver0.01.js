829684fab0621eee8ff6858002d62e3c30fcfd69
Iruka <94463133+Iruka2891@users.noreply.github.com>
2022-05-30 21:42:14 +0900

    Update ver0.01.js

--- Update/ver0.01.js
+++ Update/ver0.01.js
@@ -1,6 +1,4 @@
-    //　変数の宣言
-var access_count = 0
-var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-
-    // bodyからURLを探す
-$('body').html($('body').html().replace(exp,"<a href='$1'>$1</a>"));
+$('#MenuLink-462259').on('click', function() {
+  aler("未実装");
+  return false;
+});
